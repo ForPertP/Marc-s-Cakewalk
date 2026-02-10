5917caf62bf8412eaf561fe83f8fadec6c7a3ab5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-10 17:50:32 +0900

    Update Marc-s-Cakewalk.cpp

--- Marc-s-Cakewalk.cpp
+++ Marc-s-Cakewalk.cpp
@@ -13,19 +13,18 @@ vector<string> split(const string &);
  * The function accepts INTEGER_ARRAY calorie as parameter.
  */
 
-long marcsCakewalk(vector<int> calorie)
-{
-    long result {0};
-    
-    std::sort(calorie.begin(), calorie.end(), greater<int>());
-    
+long marcsCakewalk(vector<int> calorie) {
+    sort(calorie.begin(), calorie.end(), greater<int>());
+
+    long long miles = 0;
     for (int i = 0; i < calorie.size(); ++i)
     {
-        result += std::pow(2, i) * calorie[i];
+        miles += calorie[i] * (1LL << i);
     }
-        
-    return result;
+
+    return miles;
 }
+
 
 int main()
 {
@@ -74,28 +73,4 @@ string rtrim(const string &str)
         find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
     );
     return s;
-}
-
-std::vector<string> split(const string &str)
-{
-    vector<string> tokens;
-    string_view str_view(str);
-    size_t start = 0;
-    size_t end = 0;
-
-    while ((end = str_view.find(' ', start)) != string_view::npos)
-    {
-        if (end > start)
-        {
-            tokens.emplace_back(str_view.substr(start, end - start));
-        }
-        start = end + 1;
-    }
-
-    if (start < str_view.size())
-    {
-        tokens.emplace_back(str_view.substr(start));
-    }
-
-    return tokens;
 }
