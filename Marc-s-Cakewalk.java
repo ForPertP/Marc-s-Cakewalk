828f429e163a5f5b0dba6c4df2840d37966a0c37
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-13 16:27:00 +0900

    Add main method for Marc's Cakewalk solution

--- Marc-s-Cakewalk.java
+++ Marc-s-Cakewalk.java
@@ -31,3 +31,24 @@ class Result {
     }
 }
 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> calorie = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        long result = Result.marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
