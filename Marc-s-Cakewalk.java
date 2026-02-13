1e70c95e9b325992ba7434aebd5f4a6eec9ea913
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2026-02-13 16:26:50 +0900

    Update Marc-s-Cakewalk.java

--- Marc-s-Cakewalk.java
+++ Marc-s-Cakewalk.java
@@ -1,1 +1,33 @@
-Marc-s-Cakewalk
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<Integer> calorie) {
+        calorie.sort(Collections.reverseOrder());
+
+        long miles = 0;
+        for (int i = 0; i < calorie.size(); i++) {
+            miles += calorie.get(i) * (1L << i);
+        }
+
+        return miles;
+    }
+}
+
